9f89055b648b24604eaefbc0fb891407b0cc86b9
Doggo <80226775+DogToken@users.noreply.github.com>
2024-09-19 14:28:35 +0200

    Create newsItems.js

---
+++ src/utils/newsItems.js
@@ -0,0 +1,6 @@
+const newsItems = [
+    "Visit our featured pool: https://beta.dogswap.xyz/featured",
+    "The previous release is still available on https://old.dogswap.xyz",
+  ];
+  
+  export default newsItems;
